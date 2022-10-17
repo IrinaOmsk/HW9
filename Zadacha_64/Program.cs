eb6b0646964587724ebdbbb5322ba1ccfa52456b
Irina <I.M.ZUBKOVA@GMAIL.COM>
2022-10-17 15:18:17 +0600

    add files

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. 
+//Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+DesSeqOfNumber(n);
+
+void DesSeqOfNumber(int n)
+{
+    if (n <= 0)
+    {
+        Console.WriteLine();
+        return;
+    }
+    Console.Write($"{n} ");
+    DesSeqOfNumber(n - 1);
+}
+
+
+
---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. Рекурсивно
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите M ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+    Console.WriteLine("Введен некорректный промежуток");
+else
+    Console.WriteLine(SummNaturalElements(m, n));
+
+int SummNaturalElements(int m, int n, int summ = 0)
+{
+    if (m > n)
+    {
+        return summ;
+    }
+    summ = summ + n;
+    return SummNaturalElements(m, n - 1, summ);
+}
+
